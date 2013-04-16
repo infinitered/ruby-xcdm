07bb701146feba8f185650aef14ffcd2333f91a2
Ken Miller <ken@bitfield.net>
2013-04-16 09:55:00 -0700

    functional properties

--- lib/entity.rb
+++ lib/entity.rb
@@ -2,16 +2,56 @@
 class Entity
 
   DEFAULT_ATTRIBUTES = { :optional => 'YES', :syncable => 'YES' }
+  TYPE_MAPPING = {
+    integer16:     'Integer 16',
+    integer32:     'Integer 32',
+    integer64:     'Integer 64',
+    decimal:       'Decimal',
+    double:        'Double',
+    float:         'Float',
+    string:        'String',
+    boolean:       'Boolean',
+    datetime:      'Date',
+    binary:        'Binary Data',
+    transformable: 'Transformable'
+  }
 
-  attr_reader :name, :attributes
+
+  attr_reader :name, :properties
 
   def initialize(name, options = {})
     @name = name
-    @attributes = []
+    @properties = []
   end
 
-  def raw_attribute(options)
-    @attributes << DEFAULT_ATTRIBUTES.merge(options)
+  def raw_property(options)
+    @properties << DEFAULT_ATTRIBUTES.merge(options)
+  end
+  
+  def property(name, type, options = {})
+    property = {}
+    property[:propertyType] = self.class.convert_type(type)
+    property[:name] = name
+    options.each do |key, value|
+      value = case value
+              when false; 'NO'
+              when true; 'YES'
+              else value
+              end
+      property[key] = value
+    end
+    @properties << DEFAULT_ATTRIBUTES.merge(property)
+  end
+
+  def self.convert_type(type)
+    TYPE_MAPPING[type]
+  end
+
+  # Make shortcut property methods for each data type
+  TYPE_MAPPING.keys.each do |type|
+    define_method(type) do |name, options = {}|
+      property(name, type, options)
+    end
   end
 
 end
--- test/entity_test.rb
+++ test/entity_test.rb
@@ -4,16 +4,47 @@ require 'entity'
 
 class EntityTest < Test::Unit::TestCase
 
+  def e
+    @e ||= Entity.new("MyTest") 
+  end
+
   def test_initialize
-    e = Entity.new("MyTest")
     assert_equal "MyTest", e.name
   end
 
-  def test_raw_attribute
-    e = Entity.new("MyTest")
-    opts = { attributeType: "Integer 32", name: 'foobar' }
-    e.raw_attribute(opts)
-    assert_equal [opts.merge(optional: 'YES', syncable: 'YES')], e.attributes
+  def test_raw_property
+    opts = { propertyType: "Integer 32", name: 'foobar' }
+    e.raw_property(opts)
+    assert_equal [opts.merge(optional: 'YES', syncable: 'YES')], e.properties
+  end
+
+  def test_property_integer32
+    e.property('foobar', :integer32, optional: false)
+    assert_equal [{ optional: 'NO', syncable: 'YES', propertyType: "Integer 32", name: 'foobar' }], e.properties
+  end
+
+  def test_property_datetime
+    e.property('fazbit', :datetime, optional: false)
+    assert_equal [{ optional: 'NO', syncable: 'YES', propertyType: "Date", name: 'fazbit' }], e.properties
+  end
+
+  def test_property_short_form_string
+    e.string 'frobnoz', optional: false
+    assert_equal [{ optional: 'NO', syncable: 'YES', propertyType: "String", name: 'frobnoz' }], e.properties
+  end
+
+  def test_convert_type
+    assert_equal 'Integer 16',    Entity.convert_type(:integer16)
+    assert_equal 'Integer 32',    Entity.convert_type(:integer32)
+    assert_equal 'Integer 64',    Entity.convert_type(:integer64)
+    assert_equal 'Decimal',       Entity.convert_type(:decimal)
+    assert_equal 'Double',        Entity.convert_type(:double)
+    assert_equal 'Float',         Entity.convert_type(:float)
+    assert_equal 'String',        Entity.convert_type(:string)
+    assert_equal 'Boolean',       Entity.convert_type(:boolean)
+    assert_equal 'Date',          Entity.convert_type(:datetime)
+    assert_equal 'Binary Data',   Entity.convert_type(:binary)
+    assert_equal 'Transformable', Entity.convert_type(:transformable)
   end
 
 end
