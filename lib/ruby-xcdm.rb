0dc171aa20fc4d30cd17e3fabe549efa1e7f611c
Ken Miller <kenneth.miller@bitfield.net>
2013-07-23 13:24:48 -0700

    do not inject rake tasks if there is no schema dir

--- lib/ruby-xcdm.rb
+++ lib/ruby-xcdm.rb
@@ -3,27 +3,29 @@ require 'xcdm/entity'
 
 if defined?(Motion::Project::Config)
 
-  namespace :schema do
+  if File.directory?(File.join(App.config.project_dir, "schemas"))
+    namespace :schema do
 
-    desc "Clear the datamodel outputs"
-    task :clean do
-      files = Dir.glob(File.join(App.config.project_dir, 'resources', App.config.name) + ".{momd,xcdatamodeld}")
-      files.each do |f|
-        rm_rf f
+      desc "Clear the datamodel outputs"
+      task :clean do
+        files = Dir.glob(File.join(App.config.project_dir, 'resources', App.config.name) + ".{momd,xcdatamodeld}")
+        files.each do |f|
+          rm_rf f
+        end
+      end
+
+      desc "Generate the xcdatamodel file"
+      task :build => :clean do
+        Dir.chdir App.config.project_dir
+        runner = XCDM::Schema::Runner.new( App.config.name, "schemas", "resources")
+        App.info "Generating", "Data Model #{App.config.name}"
+        runner.load_all { |schema, file| App.info "Loading", file }
+        runner.write_all { |schema, file| App.info "Writing", file }
       end
     end
 
-    desc "Generate the xcdatamodel file"
-    task :build => :clean do
-      Dir.chdir App.config.project_dir
-      runner = XCDM::Schema::Runner.new( App.config.name, "schemas", "resources")
-      App.info "Generating", "Data Model #{App.config.name}"
-      runner.load_all { |schema, file| App.info "Loading", file }
-      runner.write_all { |schema, file| App.info "Writing", file }
-    end
+    task :"build:simulator" => :"schema:build"
+    task :"build:device" => :"schema:build"
   end
-
-  task :"build:simulator" => :"schema:build"
-  task :"build:device" => :"schema:build"
 
 end
